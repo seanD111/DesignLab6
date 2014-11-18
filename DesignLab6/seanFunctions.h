1a290e366115565639b282e3517913f91c3c7e81
seanD111 <sean_delong@hotmail.ca>
2014-11-18 14:04:06 -0500

    added to git

---
+++ DesignLab6/seanFunctions.h
@@ -0,0 +1,14 @@
+/*
+ * seanFunctions.h
+ *
+ *  Created on: Nov 18, 2014
+ *      Author: sddelong
+ */
+
+#ifndef SEANFUNCTIONS_H_
+#define SEANFUNCTIONS_H_
+
+
+
+
+#endif /* SEANFUNCTIONS_H_ */
---
+++ DesignLab6/seanImplements.cpp
@@ -0,0 +1,10 @@
+/*
+ * seanImplements.cpp
+ *
+ *  Created on: Nov 18, 2014
+ *      Author: sddelong
+ */
+
+
+
+
