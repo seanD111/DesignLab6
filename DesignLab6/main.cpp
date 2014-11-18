b9b179fdb7d9ec5c72b0ecdc755fe5e498d1c404
seanD111 <sean_delong@hotmail.ca>
2014-11-18 15:15:13 -0500

    changed strengths

--- DesignLab6/main.cpp
+++ DesignLab6/main.cpp
@@ -10,9 +10,9 @@ int main() {
 	// Example main function
 	// You may add your own testing code if you like
 
-	Scissors s1(5);
-	Paper p1(7);
-	Rock r1(15);
+	Scissors s1(3);
+	Paper p1(3);
+	Rock r1(3);
 	cout << s1.fight(p1) << p1.fight(s1) << endl;
 	cout << p1.fight(r1) << r1.fight(p1) << endl;
 	cout << r1.fight(s1) << s1.fight(r1) << endl;
