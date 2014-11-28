e5545f391a1793cd5c0f6599fec3c12992ff28b7
seanD111 <sean_delong@hotmail.ca>
2014-11-28 13:36:52 -0500

    DONNNENEEEE

--- DesignLab6/main.cpp
+++ DesignLab6/main.cpp
@@ -14,23 +14,35 @@ int main() {
 	float float_arr[5]={0.5, 1.5, 2.5, 3.5, 4.5};
 	char char_arr[8]={'a','b','c','d','e','f','g','h'};
 	string str_array[6]={"tiger", "monkey", "elephant", "rhino", "zebra", "penguin"};
-	ArrayUtils<int> iTools;
-	iTools.print(int_arr, 11);
-	iTools.printReverse(int_arr, 11);
-	cout<<iTools.sumAll(int_arr, 11)<<" " <<iTools.productOfArray(int_arr, 11)<<"\n\n";
-	ArrayUtils<float> fTools;
-	fTools.print(float_arr, 5);
-	fTools.printReverse(float_arr, 5);
-	cout<<fTools.sumAll(float_arr, 5)<<" " <<fTools.productOfArray(float_arr, 5)<<"\n\n";
-	ArrayUtils<char> cTools;
-	cTools.print(char_arr, 8);
-	cTools.printReverse(char_arr, 8);
-	cout<<cTools.sumAll(char_arr, 8)<<" " <<cTools.productOfArray(char_arr, 8)<<"\n\n";
-	ArrayUtils<string> sTools;
-	sTools.print(str_array, 6);
-	sTools.printReverse(str_array, 6);
-	//cout<<sTools.sumAll(str_array, 6)<<"\n\n";
 
+	ArrayUtils<int> iTools;
+	cout<<"Array: ";
+	iTools.print(int_arr, 11);
+	cout<<"  Reverse Array: ";
+	iTools.printReverse(int_arr, 11);
+	cout<<" Sum: "<<iTools.sumAll(int_arr, 11)<<" Product: " <<iTools.productOfArray(int_arr, 11)<<"\n\n";
+
+	ArrayUtils<float> fTools;
+	cout<<"Array: ";
+	fTools.print(float_arr, 5);
+	cout<<"  Reverse Array: ";
+	fTools.printReverse(float_arr, 5);
+	cout<<" Sum: "<<fTools.sumAll(float_arr, 5)<<" Product: " <<fTools.productOfArray(float_arr, 5)<<"\n\n";
+
+	ArrayUtils<char> cTools;
+	cout<<"Array: ";
+	cTools.print(char_arr, 8);
+	cout<<"  Reverse Array: ";
+	cTools.printReverse(char_arr, 8);
+	cout<<" Sum: "<<cTools.sumAll(char_arr, 8)<<" Product: " <<cTools.productOfArray(char_arr, 8)<<"\n\n";
+
+	ArrayUtils<string> sTools;
+	cout<<"Array: ";
+	sTools.print(str_array, 6);
+	cout<<"  Reverse Array: ";
+	sTools.printReverse(str_array, 6);
+
+	//calculate average with exceptions
 	float avg=0;
 	try {
 		avg=calculateAverage(40, 20, 30, 40);
@@ -39,6 +51,8 @@ int main() {
 	catch(OutOfRange &){
 		cerr<<"\n\nNumbers out of range!\n\n";
 	}
+
+	//testing the rock, paper, scissors classes
 	Scissors s1(3);
 	Paper p1(3);
 	Rock r1(3);
@@ -48,7 +62,7 @@ int main() {
 	cout<<endl;
 	r1.fight(s1);
 	cout<<endl;
-	s1.fight(s1);
+
 
 	return 0;
 }
